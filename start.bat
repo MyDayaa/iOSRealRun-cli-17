8e7bbe6f1303689c99b703c09c8873c3a471e7fe
MyDayaa <mydayww@outlook.com>
2024-11-25 17:44:38 +0800

    fix: bat改为相对路径执行

--- start.bat
+++ start.bat
@@ -1,3 +1,10 @@
 @echo off
-cd /d %~dp0
-powershell -Command "Start-Process powershell -ArgumentList '-NoExit', '-Command', 'cd ''C:\Softwares\iOSRealRun-cli-17''; conda activate run2; python main.py ' -Verb runAs"
+setlocal
+
+REM 获取当前批处理文件的目录
+set CURRENT_DIR=%~dp0
+
+REM 切换到当前目录并在 PowerShell 中执行命令
+powershell -Command "Start-Process powershell -ArgumentList '-NoExit', '-Command', 'cd ''%CURRENT_DIR%''; conda activate run2; python main.py' -Verb runAs"
+
+endlocal
